b0c76c2c2c1f73ebc198cd07ca97692767570827
funnygyc <764578019@qq.com>
2024-05-11 13:39:35 +0800

    gvulkan layername change

--- setEnvirOn.sh
+++ setEnvirOn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export VK_LAYER_PATH=$HOME/.config/vulkan/explicit_layer.d:/usr/share/vulkan/explicit_layer.d
-export VK_INSTANCE_LAYERS=VK_LAYER_S1MPLESONNY_remoterender
+export VK_INSTANCE_LAYERS=VK_LAYER_GVULKAN_remoterender
 
 # echo $VK_LAYER_PATH
 # echo $VK_INSTANCE_LAYERS
