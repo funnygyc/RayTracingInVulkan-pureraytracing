1f7650d2f3dd0b6cda930c8fdef3ec5111dc0eb0
funnygyc <764578019@qq.com>
2024-05-12 17:51:51 +0800

    eassy to build

--- setEnvirOn.sh
+++ setEnvirOn.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+
 export VK_LAYER_PATH=$HOME/.config/vulkan/explicit_layer.d:/usr/share/vulkan/explicit_layer.d
+
 export VK_INSTANCE_LAYERS=VK_LAYER_GVULKAN_remoterender
 
-# echo $VK_LAYER_PATH
-# echo $VK_INSTANCE_LAYERS
+echo $VK_LAYER_PATH
+echo $VK_INSTANCE_LAYERS
+
+./RayTracer > 1.txt
 
 # ./HelloTriangle > 1.log
